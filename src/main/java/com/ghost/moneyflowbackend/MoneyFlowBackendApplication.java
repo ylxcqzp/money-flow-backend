17b8d053273d1f31beaa4e2f623974c338678afa
qiuzp <2296253727@qq.com>
2026-02-04 15:25:18 +0800

    Initial commit: Money Flow Backend project setup

---
+++ src/main/java/com/ghost/moneyflowbackend/MoneyFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ghost.moneyflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyFlowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghost/moneyflowbackend/MoneyFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.moneyflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
